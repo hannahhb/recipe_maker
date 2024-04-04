7fbaec1176bde905feb3e3f5d56cc75a5619a99a
Lucas1128 <77666467+Lucas1128@users.noreply.github.com>
2024-04-04 16:45:00 +1100

    API Interactions 0.1
    Basic Functionality working

---
+++ main.py
@@ -0,0 +1,34 @@
+import requests
+
+def reciepesbyId(id):
+    baseString = 'https://api.spoonacular.com/recipes/'+str(id)+'/analyzedInstructions?apiKey=79001ad4da024048843e722ecdb550ef'
+    
+    APIresponse = requests.get(baseString).json()
+    APIresponse = APIresponse[0]["steps"]
+
+    instructions = []
+    for step in APIresponse:
+        instructions.append(step['step'])
+    
+    return instructions
+    
+def findRecipes(ingredients):
+    baseString = 'https://api.spoonacular.com/recipes/findByIngredients?ingredients='
+
+    for ingredient in ingredients:
+        baseString += ingredient +',+'
+    
+    baseString = baseString[:-2] + '&number=3&apiKey=79001ad4da024048843e722ecdb550ef'
+
+    APIresponse = requests.get(baseString).json()
+
+    dishOptions = []
+
+    for dish in APIresponse:
+        instructions = reciepesbyId(dish['id'])
+        dishOptions.append((dish['id'], dish['title'], dish['image'], instructions))
+
+    return dishOptions
+
+print(findRecipes(['apples', 'flour', 'vanilla', 'sugar']))
+
